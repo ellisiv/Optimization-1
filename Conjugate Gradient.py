742475a12b2b106067fa0c94a03c3d1c44db288a
Ellisiv <ellisiv_s_s@hotmail.com>
2019-03-09 16:27:53 +0100

    meningsløs versjon

--- Conjugate Gradient.py
+++ Conjugate Gradient.py
@@ -8,9 +8,8 @@ def r1(zi, A, c):
     return np.dot(zic, (zi - c)) - 1
 
 
-
 def f1(x, z, inner):
-    A = np.zeros((2,2))
+    A = np.zeros((2, 2))
     A[0] = [x[0], x[1]]
     A[2] = [x[1], x[2]]
     c = x[-2:]
@@ -54,7 +53,8 @@ def grad1(x, z, inner):
         if (i in inner and r > 0) or (i not in inner and r < 0):
             g1 += 2 * r * np.matmul((z[i] - c), (z[i] - c).transpose())
             g2 += -4 * r * np.dot(np.array([1, 2]).transpose(), np.eye(2))
-    return g1, g2
+    g = np.array([g1[0, 0], g1[0, 1], g1[1, 1], g2[0], g2[1]])
+    return g
 
 def grad2(x, z, inner):
     g1 = np.zeros((2, 2))
@@ -68,7 +68,8 @@ def grad2(x, z, inner):
         if (i in inner and r > 0) or (i not in inner and r < 0):
             g1 += 2 * r * np.matmul(z[i], z[i].T)
             g2 += -2 * r * z[i].T
-    return g1, g2
+    g = np.array([g1[0,0], g1[0,1], g1[1, 1], g2[0], g2[1]])
+    return g
 
 
 def linesearch_wolfe(z, inner, p, x, c1=10 ** -4, c2 = 0.9, text= "1"):
@@ -100,27 +101,42 @@ def linesearch_wolfe(z, inner, p, x, c1=10 ** -4, c2 = 0.9, text= "1"):
     return alpha
 
 
-def BFGS(x, z, inner, f1, f2, text):
+def BFGS(x, z, inner, text):
     H = np.eye(2)
     xnew = x
-    xold = np.infty
+    xold = np.array(5 * [np.infty])
     if text == "1":
-        p = -H * grad1(x, z, inner)
-        alpha = linesearch_wolfe(z, inner, p, x, text="1")
-        xold = xnew
-        xnew = xnew + alpha * p
-        s = xnew - xold
-        y = grad1(xnew, z, inner) - grad1(xold, z, inner)
-        ysk = np.dot(y.T)
-        H =
-
+        while np.linalg.norm(xnew - xold, 2) < 10 ** -8:
+            p = -H * grad1(x, z, inner)
+            alpha = linesearch_wolfe(z, inner, p, x, text="1")
+            xold = xnew
+            xnew = xnew + alpha * p
+            s = xnew - xold
+            y = grad1(xnew, z, inner) - grad1(xold, z, inner)
+            ysk = np.dot(y.T,s)
+            H =np.matmul(np.matmul((np.eye(2)- 1 / ysk * np.dot(s, y.T)), H), (np.eye(2)- 1 / ysk * np.dot(y, s.T)))
     else:
+        while np.linalg.norm(xnew - xold, 2) < 10 ** -8:
+            p = -H * grad2(x, z, inner)
+            alpha = linesearch_wolfe(z, inner, p, x, text="2")
+            xold = xnew
+            xnew = xnew + alpha * p
+            s = xnew - xold
+            y = grad2(xnew, z, inner) - grad2(xold, z, inner)
+            ysk = np.dot(y.T, s)
+            H = np.matmul(np.matmul((np.eye(2) - 1 / ysk * np.dot(s, y.T)), H), (np.eye(2) - 1 / ysk * np.dot(y, s.T)))
+    return x
 
-    #initialize B0
-    #compute pk = -Hk*gradf(xk) here we split up x
-    #line search alpha
-    #xk+1 = xk +alphakpk:
-        #sk = xk+1-xk
-        #yk = grad f(xk+1) - grad f(xk)
-        #Bk+1 = ...
-    return A, b
+c = np.array([2, 2])
+A = 3 * np.eye(2)
+
+x = np.array([3, 1, 3, 2, 2])
+
+points = np.random.multivariate_normal(c, 1 * np.linalg.inv(A), size=90)
+
+inner = []
+for i in range(len(points)):
+    if r1(points[i], A, c) <= 0:
+        inner.append(i)
+
+print(BFGS(x, points, inner, "1"))
