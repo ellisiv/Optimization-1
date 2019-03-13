2d0500154ba2971db628c3356f22d363cbad3ead
Ellisiv <ellisiv_s_s@hotmail.com>
2019-03-13 10:08:13 +0100

    En kode som ish funker!

--- Conjugate Gradient.py
+++ Conjugate Gradient.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.linalg
+from matplotlib import patches
 
 
 def r1(zi, A, c):
-    zic = np.matmul((zi - c).transpose(), A)
-    return np.dot(zic, (zi - c)) - 1
+    zic = np.matmul((zi - c).T, A)
+    return np.matmul(zic, (zi - c)) - 1
 
 
 def f1(x, z, inner):
     A = np.zeros((2, 2))
     A[0] = [x[0], x[1]]
-    A[2] = [x[1], x[2]]
-    c = x[-2:]
+    A[1] = [x[1], x[2]]
+    c = [x[3], x[4]]
     sum = 0
     for i in range(len(z)):
         if i in inner:
@@ -24,13 +25,13 @@ def f1(x, z, inner):
 
 def r2(zi, A, b):
     zib = np.matmul(zi.transpose(), A)
-    return np.dot(zib, zi) - np.dot(zi.transpose(), b) - 1
+    return np.matmul(zib, zi) - np.matmul(zi.transpose(), b) - 1
 
 
 def f2(x, z, inner):
     A = np.zeros((2, 2))
     A[0] = [x[0], x[1]]
-    A[2] = [x[1], x[2]]
+    A[1] = [x[1], x[2]]
     b = x[-2:]
     sum = 0
     for i in range(len(z)):
@@ -47,12 +48,12 @@ def grad1(x, z, inner):
     A = np.zeros((2, 2))
     A[0] = [x[0], x[1]]
     A[1] = [x[1], x[2]]
-    c = x[-2:]
+    c = [x[3], x[4]]
     for i in range(len(z)):
         r = r1(z[i], A, c)
-        if (i in inner and r > 0) or (i not in inner and r < 0):
+        if (i in inner and r > 0) or ((i not in inner) and r < 0):
             g1 += 2 * r * np.matmul((z[i] - c), (z[i] - c).transpose())
-            g2 += -4 * r * np.dot(np.array([1, 2]).transpose(), np.eye(2))
+            g2 += -4 * r * (z[i].T - c) @ A
     g = np.array([g1[0, 0], g1[0, 1], g1[1, 1], g2[0], g2[1]])
     return g
 
@@ -68,17 +69,36 @@ def grad2(x, z, inner):
         if (i in inner and r > 0) or (i not in inner and r < 0):
             g1 += 2 * r * np.matmul(z[i], z[i].T)
             g2 += -2 * r * z[i].T
-    g = np.array([g1[0,0], g1[0,1], g1[1, 1], g2[0], g2[1]])
+    g = np.array([g1[0,0], g1[0, 1], g1[1, 1], g2[0], g2[1]])
     return g
 
 
-def linesearch_wolfe(z, inner, p, x, c1=10 ** -4, c2 = 0.9, text= "1"):
+def linesearch_wolfe(z, inner, p, x, c1=10 ** -4, c2=0.9, text="1"):
     alpha = 1
     amax = np.infty
     amin = 0
     if text == "1":
-        while (f1((x + alpha * p), z, inner) > f1(x, z, inner) + c1 * alpha * grad1(x, z , inner)) and (np.dot(grad1(x + alpha * p, z, inner).T, p) < c2 * np.dot(grad1(x, z, inner).T, p)):
-            if f1(x + alpha * p, z, inner) > f1(x, z, inner) + c1 * alpha * grad1(x, z, inner):
+        while (f1((x + alpha * p), z, inner) > f1(x, z, inner) + c1 * alpha * np.matmul(grad1(x, z, inner).T, p)) or \
+                (np.matmul(grad1(x + alpha * p, z, inner).T, p) < c2 * np.matmul(grad1(x, z, inner).T, p)):
+            if f1(x + alpha * p, z, inner) > f1(x, z, inner) + c1 * alpha * np.matmul(grad1(x, z, inner).T, p):
+                if alpha < 10 ** (-8):
+                    return alpha
+                #print(alpha)
+                amax = alpha
+                alpha = (amax + amin) / 2
+            else:
+                #print(alpha)
+                amin = alpha
+                if alpha < 10 ** (-8):
+                    return alpha
+                if amax == np.infty:
+                    alpha = alpha * 2
+                else:
+                    alpha = (amax + amin)/2
+    else:
+        while (f2(x + alpha * p, z, inner) > f2(x, z, inner) + c1 * alpha * np.matmul(grad2(x, z, inner), p)) and \
+                (np.matmul(grad2(x + alpha * p, z, inner).T, p) < c2 * np.matmul(grad2(x, z, inner).T, p)):
+            if f2(x + alpha * p, z, inner) > f2(x, z, inner) + c1 * alpha * np.matmul(grad2(x, z, inner), p):
                 amax = alpha
                 alpha = (amax + amin) / 2
             else:
@@ -87,56 +107,83 @@ def linesearch_wolfe(z, inner, p, x, c1=10 ** -4, c2 = 0.9, text= "1"):
                     alpha = alpha * 2
                 else:
                     alpha = (amax + amin)/2
-    else:
-        while (f2(x + alpha * p, z, inner) > f2(x, z, inner) + c1 * alpha * grad2(x, z, inner)) and (np.dot(grad2(x + alpha * p, z, inner).T, p) < c2 * np.dot(grad2(x, z, inner).T, p)):
-            if f2(x + alpha * p, z, inner) > f2(x, z, inner) + c1 * alpha * grad2(x, z, inner):
-                amax = alpha
-                alpha = (amax + amin) / 2
-            else:
-                amin = alpha
-                if amax == np.infty:
-                    alpha = alpha*2
-                else:
-                    alpha = (amax + amin)/2
     return alpha
 
 
+def get_ellipse(A, c):
+    eigen_values, eigen_vectors = np.linalg.eig(A)
+    principal_axes = np.sqrt(1 / eigen_values)
+
+    vec1 = eigen_vectors[0, :]
+    angle = np.arctan(vec1[0] / vec1[1])
+
+    e1 = patches.Ellipse((c[0], c[1]), 2 * principal_axes[0], 2 * principal_axes[1],
+                         angle=-angle * 180 / np.pi, linewidth=2, fill=False, zorder=2)
+    return e1
+
+
 def BFGS(x, z, inner, text):
-    H = np.eye(2)
+    H = np.eye(5)
     xnew = x
     xold = np.array(5 * [np.infty])
+    n = 0
+    Af = np.zeros((2, 2))
+    Af[0] = [xnew[0], xnew[1]]
+    Af[1] = [xnew[1], xnew[2]]
+    cf = xnew[-2:]
+    plt.figure()
+    plt.scatter(points[:, 0], points[:, 1])
+    plt.scatter(points[inner, 0], points[inner, 1])
+    ax = plt.axes()
+    ax.add_patch(get_ellipse(Af, cf))
+    plt.show()
     if text == "1":
-        while np.linalg.norm(xnew - xold, 2) < 10 ** -8:
-            p = -H * grad1(x, z, inner)
-            alpha = linesearch_wolfe(z, inner, p, x, text="1")
+        while np.linalg.norm(grad1(xnew, z, inner), 2) > 10 ** (-3) and n < 5:
+            p = - np.matmul(H, grad1(xnew, z, inner))
+            print(p)
+            alpha = linesearch_wolfe(z, inner, p, xnew, text="1")
             xold = xnew
             xnew = xnew + alpha * p
             s = xnew - xold
             y = grad1(xnew, z, inner) - grad1(xold, z, inner)
-            ysk = np.dot(y.T,s)
-            H =np.matmul(np.matmul((np.eye(2)- 1 / ysk * np.dot(s, y.T)), H), (np.eye(2)- 1 / ysk * np.dot(y, s.T)))
-    else:
-        while np.linalg.norm(xnew - xold, 2) < 10 ** -8:
-            p = -H * grad2(x, z, inner)
-            alpha = linesearch_wolfe(z, inner, p, x, text="2")
-            xold = xnew
-            xnew = xnew + alpha * p
-            s = xnew - xold
-            y = grad2(xnew, z, inner) - grad2(xold, z, inner)
-            ysk = np.dot(y.T, s)
-            H = np.matmul(np.matmul((np.eye(2) - 1 / ysk * np.dot(s, y.T)), H), (np.eye(2) - 1 / ysk * np.dot(y, s.T)))
-    return x
+            ysk = np.matmul(y.T, s)
+            if n == 0:
+                H = np.matmul(y.T, s) / np.matmul(y.T, s) * H
+            H = np.matmul(np.matmul((np.eye(5) - 1 / ysk * np.matmul(s, y.T)), H), (np.eye(5) - 1 / ysk * np.matmul(y, s.T)))
+            Af = np.zeros((2, 2))
+            Af[0] = [xnew[0], xnew[1]]
+            Af[1] = [xnew[1], xnew[2]]
+            cf = xnew[-2:]
+            n += 1
+    return xnew
 
 c = np.array([2, 2])
 A = 3 * np.eye(2)
+A[0, 1], A[1, 0] = 1, 1
 
-x = np.array([3, 1, 3, 2, 2])
+x0 = np.array([3, 1, 3, 1.6, 2])
 
-points = np.random.multivariate_normal(c, 1 * np.linalg.inv(A), size=90)
+points = np.random.multivariate_normal(c, 1 * np.linalg.inv(A), size=500)
 
 inner = []
 for i in range(len(points)):
     if r1(points[i], A, c) <= 0:
         inner.append(i)
 
-print(BFGS(x, points, inner, "1"))
+xf = BFGS(x0, points, inner, "1")
+print(xf)
+Af = np.zeros((2, 2))
+Af[0] = [xf[0], xf[1]]
+Af[1] = [xf[1], xf[2]]
+cf = xf[-2:]
+
+
+
+plt.figure()
+plt.scatter(points[:, 0], points[:, 1])
+plt.scatter(points[inner, 0], points[inner, 1])
+ax = plt.axes()
+ax.add_patch(get_ellipse(Af, cf))
+plt.show()
+
+
